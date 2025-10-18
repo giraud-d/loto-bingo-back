9ed728bc30f0aac27b58c3ad367d7ba96de04559
damien <damien@giraudet.me>
2025-10-18 21:46:39 +0200

    core: init spring boot

---
+++ src/main/java/me/giraudet/lotobingo/LotoBingoApplication.java
@@ -0,0 +1,13 @@
+package me.giraudet.lotobingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotoBingoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotoBingoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/me/giraudet/lotobingo/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package me.giraudet.lotobingo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String getHelloWorld() {
+        return "Hello World!";
+    }
+}
